30a13f5898b49ba4bd449520431c72a145ff55f3
Alex Ly <alexly@Alexs-MBP.home>
2016-03-13 03:00:20 -0400

    starting to add routes, need to test mongoose.

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// MIDDLEWARE
+
+
+// ROUTES
+
+app.listen(PORT, function(){
+  console.log('Listening on port ',PORT);
+})
