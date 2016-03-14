3e9f3245e77500a786b48b1b52619d8371e0fc4d
Alex Ly <alexly@Alexs-MBP.home>
2016-03-14 05:35:44 -0400

    Scraping works, being passed into DB, but need restart to view saved content. Need to fix this.

--- controller/routes.js
+++ controller/routes.js
@@ -13,10 +13,21 @@ router.get('/', function(req, res){
 
 // Enterpreneur scrape route
 router.post('/entrepreneur-scrape', function(req,res){
-  var entTitle = {title:'The muffin man', link: 'https://www.youtube.com/watch?v=t4iu0j5BtAs'};
-  var entrep = new Entrepreneur(entTitle);
+  
+  request('https://www.reddit.com/r/entrepreneur/', function(err, response, stuff){
+    var data = cheerio.load(stuff);
+    data('.title').each(function(i, element){
+      var title = data(this).children('a').text();
+      var link = data(this).children('a').attr('href');
 
-  entrep.save(entTitle);
+      if(title && link){
+        var entTitle = {title, link};
+        var entrep = new Entrepreneur(entTitle);
+        entrep.save(entTitle);
+      }
+    });
+  });  
+  
   res.redirect('/entrepreneur');
 });
 
