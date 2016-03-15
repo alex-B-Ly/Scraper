89a9a5b9fa3e9b3830d7f4ccb71473501fc47f49
Alex Ly <alexly@Alexs-MBP.home>
2016-03-15 04:45:58 -0400

    continuing to build out the title link and comments front end functionality

--- public/js/functionality.js
+++ public/js/functionality.js
@@ -11,17 +11,28 @@ $(document).ready(function(){
     for(var i = 0; i<12; i++){
       var titleLi = $('<li>');
       var titleLink = $('<a>');
+      var deleteButton = $('<button>');
 
+      // Generate link title
       titleLink.text(data[i].title)
         .attr('href', data[i].link)
         .addClass('title');
 
+      // Generate delete button
+      deleteButton.text('X')
+        .addClass('pull-right btn btn-sm btn-default delete-title');
+
+      // Generate list item with data attribute
       titleLi.append(titleLink)
         .attr('data-id', data[i]._id)
-        .addClass('list-group-item');
+        .addClass('list-group-item title-item col-xs-12');
+
+      titleLi.append(deleteButton);
 
       $('#content-list').append(titleLi);
     }
   }
+
+  // COMMENTS BUILDER
 
 });
